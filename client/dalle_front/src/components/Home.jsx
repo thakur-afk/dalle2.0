3c683310cedcc567f037d21c1200a7259ce83050
Harshit Thakur <harshitthakur@ternaengg.ac.in>
2024-01-11 20:42:19 +0530

    changes in client side

--- client/dalle_front/src/components/Home.jsx
+++ client/dalle_front/src/components/Home.jsx
@@ -36,12 +36,15 @@ const Home = () => {
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://dalle-backend-gu1c.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
