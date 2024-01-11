812e413c322cea8187df7db4c691cb856d71e34b
Harshit Thakur <harshitthakur@ternaengg.ac.in>
2024-01-11 17:35:20 +0530

    second commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,15 +1,11 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import OpenAi from "openai";
+
 import fetch from "node-fetch";
 
 dotenv.config();
 
 const router = express.Router();
-
-const openai = new OpenAi({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 router.route("/").get((req, res) => {
   res.send("Hello from dalle clone");
